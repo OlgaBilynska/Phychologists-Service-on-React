e34b6c62df152cc39758d362309f9ad206e12c71
OlgaBilynska <122179645+OlgaBilynska@users.noreply.github.com>
2024-01-07 21:51:33 +0000

    Deploying to gh-pages from @ OlgaBilynska/Phychologists-Service-on-React@6a1442271b95b96beb752a0f545ea01b2bf95768 🚀

---
+++ static/js/681.f59b98d8.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[681],{3681:function(e,t,r){r.r(t),r.d(t,{default:function(){return a}});var n=r(184),a=function(){return(0,n.jsx)(n.Fragment,{children:"FavoritePage"})}}}]);
+//# sourceMappingURL=681.f59b98d8.chunk.js.map
